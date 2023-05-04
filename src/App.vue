265f1eb0579b8c2ba4a80a08229e36e0914608a4
Obvium <32954603+Obvium@users.noreply.github.com>
2023-05-04 21:18:06 +0200

    does this work

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,89 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="font-bold">
+      Test?s
     </div>
+    <div class="font-bold m-5 text-lg">
+      {{ getText() }}
+    </div>
+    <button 
+      @click= setCities()
+    >Text</button>
+    <button class="m-10"
+      @click= setCities()
+    >Sign In</button>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { collection, doc, setDoc, getFirestore } from 'firebase/firestore';
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyALV08bfPkleXurrp4q-ZZqLB1buSChfhU",
+  authDomain: "restlist-ab369.firebaseapp.com",
+  projectId: "restlist-ab369",
+  storageBucket: "restlist-ab369.appspot.com",
+  messagingSenderId: "1032725124554",
+  appId: "1:1032725124554:web:304b9b702ca638157aab33",
+  measurementId: "G-7FGCH3NTKT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+const email = "tobias.topar@gmail.com"
+const password = "Test1234$"
+console.log("now logging in");
+// signInWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//   });
+
+console.log("hello?");
+function getText() {
+  return "hellsafao";
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+
+async function setCities() {
+  console.log("Pressed");
+  const db = getFirestore(app);
+  const citiesRef = collection(db, "cities");
+
+  await setDoc(doc(citiesRef, "SF"), {
+      name: "Sanaggo", state: "CA", country: "USA",
+      capital: false, population: 860000,
+      regions: ["west_coast", "norcal"] });
+  await setDoc(doc(citiesRef, "LA"), {
+      name: "Loseles", state: "CA", country: "USA",
+      capital: false, population: 3900000,
+      regions: ["west_coast", "socal"] });
+  await setDoc(doc(citiesRef, "DC"), {
+      name: "Washington, D.C.", state: null, country: "USA",
+      capital: true, population: 680000,
+      regions: ["east_coast"] });
+  await setDoc(doc(citiesRef, "TOK"), {
+      name: "Tokyo", state: null, country: "Japan",
+      capital: true, population: 9000000,
+      regions: ["kanto", "honshu"] });
+  await setDoc(doc(citiesRef, "BJ"), {
+      name: "Beijing", state: null, country: "China",
+      capital: true, population: 21500000,
+      regions: ["jingjinji", "hebei"] });
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
