65123aa7ffd9bba6d2265cca62d3cf433f2c49ef
Obvium <32954603+Obvium@users.noreply.github.com>
2023-05-05 15:42:56 +0200

    i did things

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,134 @@
 <template>
-  <header>
-    <div class="font-bold">
-      Test?s
+  <div class="h-screen bg-slate-900 p-20">
+    <div
+      class="border bg-slate-500 max-w-"
+    >
+      <div v-if="!loggedIn">
+        <input 
+          class="mt-3 ml-5 rounded bg-slate-100 border w-[calc(100%-2.5rem)] font-bold"
+          v-model="email" 
+          placeholder="E-Mail"
+        />     
+        <input 
+          class="ml-5 mt-1 rounded bg-slate-100 border w-[calc(100%-2.5rem)]"  
+          type="password" 
+          v-model="password" 
+          placeholder="Password" 
+        />
+      </div>
+      <div class="flex ">
+        <button v-if="!loggedIn" class="p-1 mx-[30%] my-3 w-full rounded-xl bg-slate-100" @click= logIn()>Log In</button>
+        <button v-if="loggedIn" class="p-1 mx-[30%] my-3 w-full rounded-xl bg-slate-100" @click= logOut()>Log Out</button>
+      </div>
+
     </div>
-    <div class="font-bold m-5 text-lg">
-      {{ getText() }}
-    </div>
-    <button 
-      @click= setCities()
-    >Text</button>
-    <button class="m-10"
-      @click= setCities()
-    >Sign In</button>
-  </header>
+
+  </div>
 </template>
 
-<script setup>
-// Import the functions you need from the SDKs you need
+<script>
 import { initializeApp } from "firebase/app";
 import { collection, doc, setDoc, getFirestore } from 'firebase/firestore';
-import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import {getAuth, signOut, signInWithEmailAndPassword , onAuthStateChanged} from "firebase/auth";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      auth: "",
+      user: "",
+      loggedIn: false,
+    }
+  },
+  created() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyALV08bfPkleXurrp4q-ZZqLB1buSChfhU",
+      authDomain: "restlist-ab369.firebaseapp.com",
+      projectId: "restlist-ab369",
+      storageBucket: "restlist-ab369.appspot.com",
+      messagingSenderId: "1032725124554",
+      appId: "1:1032725124554:web:304b9b702ca638157aab33",
+      measurementId: "G-7FGCH3NTKT"
+    };
+    const app = initializeApp(firebaseConfig);
+
+    this.auth = getAuth();
+    onAuthStateChanged(this.auth, (user) => {
+      if (user) {
+        //console.log("Signed in");
+        this.loggedIn = true;
+        const uid = user.uid;
+      } else {
+        //console.log("Signed out");
+        this.loggedIn = false;
+      }
+    });
+
+  },
+  methods: {
+    logIn() {
+      signInWithEmailAndPassword(this.auth, this.email, this.password)
+          .then((userCredential) => {
+            this.user = userCredential.user;
+          })
+          .catch((error) => {
+            this.errorCode = error.code;
+            this.errorMessage = error.message;
+          });
+      console.log(this.email);
+      console.log(this.password);
+    },
+    logOut() {
+      signOut(this.auth).then(() => {
+       }).catch((error) => {
+    });
+    }
+
+  }
+}
+// Import the functions you need from the SDKs you need
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyALV08bfPkleXurrp4q-ZZqLB1buSChfhU",
-  authDomain: "restlist-ab369.firebaseapp.com",
-  projectId: "restlist-ab369",
-  storageBucket: "restlist-ab369.appspot.com",
-  messagingSenderId: "1032725124554",
-  appId: "1:1032725124554:web:304b9b702ca638157aab33",
-  measurementId: "G-7FGCH3NTKT"
-};
+
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
-const auth = getAuth();
-const email = "tobias.topar@gmail.com"
-const password = "Test1234$"
-console.log("now logging in");
-// signInWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // Signed in 
-//     const user = userCredential.user;
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//   });
+
 
 console.log("hello?");
-function getText() {
-  return "hellsafao";
-}
 
 
-async function setCities() {
-  console.log("Pressed");
-  const db = getFirestore(app);
-  const citiesRef = collection(db, "cities");
 
-  await setDoc(doc(citiesRef, "SF"), {
-      name: "Sanaggo", state: "CA", country: "USA",
-      capital: false, population: 860000,
-      regions: ["west_coast", "norcal"] });
-  await setDoc(doc(citiesRef, "LA"), {
-      name: "Loseles", state: "CA", country: "USA",
-      capital: false, population: 3900000,
-      regions: ["west_coast", "socal"] });
-  await setDoc(doc(citiesRef, "DC"), {
-      name: "Washington, D.C.", state: null, country: "USA",
-      capital: true, population: 680000,
-      regions: ["east_coast"] });
-  await setDoc(doc(citiesRef, "TOK"), {
-      name: "Tokyo", state: null, country: "Japan",
-      capital: true, population: 9000000,
-      regions: ["kanto", "honshu"] });
-  await setDoc(doc(citiesRef, "BJ"), {
-      name: "Beijing", state: null, country: "China",
-      capital: true, population: 21500000,
-      regions: ["jingjinji", "hebei"] });
-}
+
+// async function setCities() {
+//   console.log("Pressed");
+//   const db = getFirestore(app);
+//   const citiesRef = collection(db, "cities");
+
+//   await setDoc(doc(citiesRef, "SF"), {
+//       name: "Sanaggo", state: "CA", country: "USA",
+//       capital: false, population: 860000,
+//       regions: ["west_coast", "norcal"] });
+//   await setDoc(doc(citiesRef, "LA"), {
+//       name: "Loseles", state: "CA", country: "USA",
+//       capital: false, population: 3900000,
+//       regions: ["west_coast", "socal"] });
+//   await setDoc(doc(citiesRef, "DC"), {
+//       name: "Washington, D.C.", state: null, country: "USA",
+//       capital: true, population: 680000,
+//       regions: ["east_coast"] });
+//   await setDoc(doc(citiesRef, "TOK"), {
+//       name: "Tokyo", state: null, country: "Japan",
+//       capital: true, population: 9000000,
+//       regions: ["kanto", "honshu"] });
+//   await setDoc(doc(citiesRef, "BJ"), {
+//       name: "Beijing", state: null, country: "China",
+//       capital: true, population: 21500000,
+//       regions: ["jingjinji", "hebei"] });
+// }
 </script>
